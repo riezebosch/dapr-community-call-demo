3ae6df18a9b99db68ffcd0f2701e377c448c0611
Manuel Riezebosch <mriezebosch@gmail.com>
2022-03-08 11:51:31 +0100

    feat(cmd): use cloud events and connect to dapr

--- service-b/ServiceB.Cmd/Program.cs
+++ service-b/ServiceB.Cmd/Program.cs
@@ -1,4 +1,6 @@
-﻿using MassTransit;
+﻿using System.Net.Mime;
+using CloudEventify.MassTransit;
+using MassTransit;
 using ServiceB;
 
 var bus = Bus.Factory
@@ -8,7 +10,16 @@ var bus = Bus.Factory
         cfg.ReceiveEndpoint("serviceb-requests", x =>
         {
             x.Consumer<Consumer>();
+            x.Bind("requests");
+
+            x.UseCloudEvents()
+                .WithContentType(new ContentType("text/plain"));
         });
+        
+        cfg.Message<Approved>(x => 
+            x.SetEntityName("requests/approved"));
+
+        cfg.UseCloudEvents();
     });
 
 await bus.StartAsync();
